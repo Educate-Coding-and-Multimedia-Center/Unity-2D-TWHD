b4818b1d086244accd42ab453de8a8eaadf7da7b
E-ducate Pontianak <storage.educate@gmail.com>
2020-09-01 10:46:25 +0700

    Adding TWHD project

---
+++ Assets/Scripts/EnemyRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotate : MonoBehaviour {
+
+	public GameObject pivot;
+	public float rotateSpeed = 50f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(pivot.transform.position, Vector3.forward, rotateSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+
+    void FixedUpdate() {
+
+        Vector2 temp = transform.position;
+
+        if(Input.GetAxis("Horizontal") > 0) {
+
+            temp.x += moveSpeed * Time.deltaTime;
+        
+        } else if (Input.GetAxis("Horizontal") < 0) {
+
+            temp.x -= moveSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetAxis("Vertical") > 0) {
+
+            temp.y += moveSpeed * Time.deltaTime;
+
+        } else if (Input.GetAxis("Vertical") < 0) {
+
+            temp.y -= moveSpeed * Time.deltaTime;
+
+        }
+
+        transform.position = temp;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D target) {
+
+        if(target.tag == "Enemy") {
+            gameObject.SetActive(false);
+            Invoke("RestartGame", 2f);
+        }
+
+        if (target.tag == "Goal") {
+            NextLevel();
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
